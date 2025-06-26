cc7005b377b5902f9be49312c06052925b01ecdd
yumnie <yum10@outlook.com>
2025-06-26 19:06:44 +0200

    fix deploy

--- src/dao/dbconnection.js
+++ src/dao/dbconnection.js
@@ -1,19 +1,15 @@
-const mysql = require('mysql2')
-require('dotenv').config()
+const mysql = require('mysql2');
+require('dotenv').config();
 
-var pool = mysql.createPool({
+const pool = mysql.createPool({
     connectionLimit: 10,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-})
-
-module.exports = pool
-
-
-
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'root',
+    database: process.env.DB_DATABASE || 'share-a-meal',
+    multipleStatements: true
+});
 
 pool.on('acquire', function (connection) {
   console.log('Connection %d acquired', connection.threadId);
@@ -22,3 +18,5 @@ pool.on('acquire', function (connection) {
 pool.on('release', function (connection) {
   console.log('Connection %d released', connection.threadId);
 });
+
+module.exports = pool;
