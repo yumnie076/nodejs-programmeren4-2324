496c8fc8e18e3410c7856b79f86df6f19b64a21a
yumnie <yum10@outlook.com>
2025-06-26 15:50:55 +0200

    Update meal.test.js

--- test/meal.test.js
+++ test/meal.test.js
@@ -1,246 +1,137 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const server = require('../index');
-const tracer = require('tracer');
-const { expect } = chai;
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const server = require('../index')
+const tracer = require('tracer')
+const expect = chai.expect
+const assert = require('assert')
+const jwt = require('jsonwebtoken')
+const jwtSecretKey = require('../src/util/config').secretkey
+chai.should()
+chai.use(chaiHttp)
+tracer.setLevel('warn')
 
-chai.should();
-chai.use(chaiHttp);
-tracer.setLevel('warn');
+const endpointToTest = '/api/meal'
 
-// Maak testgebruiker aan
-before((done) => {
-    chai.request(server)
-        .post('/api/user')
-        .send({
-            firstName: 'Test',
-            lastName: 'User',
-            emailAddress: 'test@meal.nl',
-            password: 'Secret123!',
-            phoneNumber: '0612345678',
-            street: 'Teststraat',
-            city: 'Teststad',
-            roles: 'guest'
-        })
-        .end(() => done()); // Negeert duplicate error
-});
+describe('UC301 toevoegen van maaltijd', () => {
+    beforeEach((done) => {
+        done()
+    })
 
-function getValidToken(callback) {
-    chai.request(server)
-        .post('/api/login')
-        .send({ emailAddress: 'test@meal.nl', password: 'Secret123!' })
-        .end((err, res) => {
-            if (err || !res.body.data || !res.body.data.token) {
-                return callback(new Error('Token niet ontvangen'));
-            }
-            callback(null, res.body.data.token);
-        });
-}
+    it('TC-301-1 Verplicht veld ontbreekt bij toevoegen maaltijd', (done) => {
+        const token = jwt.sign({ id: 1 }, jwtSecretKey, { expiresIn: '1h' })
 
-describe('UC-301 Add Meal', () => {
-    it('TC-301-1 Required field missing', (done) => {
-        getValidToken((err, token) => {
-            if (err) return done(err);
-            chai.request(server)
-                .post('/api/meals')
-                .set('Authorization', `Bearer ${token}`)
-                .send({ name: 'Pizza' })
-                .end((err, res) => {
-                    expect(res).to.have.status(400);
-                    done();
-                });
-        });
-    });
+        // Maaltijdgegevens zonder het verplichte veld 'name'
+        const incompleteMealData = {
+            description: 'Heerlijke pasta met bolognesesaus',
+            price: 8.5,
+            dateTime: '2024-05-26 18:00:00',
+            maxAmountOfParticipants: 10,
+            imageUrl: 'https://example.com/image.jpg',
+            isActive: true,
+            isVega: false,
+            isVegan: false,
+            isToTakeHome: true,
+            allergenes: 'gluten'
+        }
 
-    it('TC-301-2 Not logged in', (done) => {
         chai.request(server)
-            .post('/api/meals')
-            .send({ name: 'Pizza', description: 'Delicious cheese pizza', price: 9.99 })
+            .post('/api/meal')
+            .set('Authorization', `Bearer ${token}`)
+            .send(incompleteMealData)
             .end((err, res) => {
-                expect(res).to.have.status(401);
-                expect(res.body).to.have.property('message').that.is.a('string');
-                expect(res.body.message).to.equal('Authorization header missing!');
-                done();
-            });
-    });
+                if (err) {
+                    console.error('Error creating meal:', err)
+                } else {
+                    console.log('Create meal response:', res.body)
+                }
 
-    it('TC-301-3 Meal successfully added', (done) => {
-        getValidToken((err, token) => {
-            if (err) return done(err);
-            chai.request(server)
-                .post('/api/meals')
-                .set('Authorization', `Bearer ${token}`)
-                .send({
-                    name: 'Pizza',
-                    description: 'Cheese pizza',
-                    price: 9.99,
-                    maxAmountOfParticipants: 5,
-                    dateTime: '2024-06-22T12:00:00'
-                })
-                .end((err, res) => {
-                    expect(res).to.have.status(201);
-                    done();
-                    console.log('LOGIN RESPONSE', res.body);
-                });
-        });
-    });
-});
+                // We verwachten een 400 status omdat een verplicht veld ontbreekt
+                expect(res).to.have.status(400)
+                expect(res.body).to.be.an('object')
+                expect(res.body).to.have.property('message').that.is.a('string')
+                expect(res.body.message).to.equal(
+                    'Missing required field(s): name'
+                )
+                expect(res.body).to.have.property('data').that.is.empty
+                done()
+            })
+    })
 
-describe('UC-302 Wijzigen van maaltijdsgegevens', () => {
-    it('TC-302-1 Verplicht velden ontbreken', (done) => {
-        getValidToken((err, token) => {
-            if (err) return done(err);
-            chai.request(server)
-                .put('/api/meals/1')
-                .set('Authorization', `Bearer ${token}`)
-                .send({ name: 'Pizza' })
-                .end((err, res) => {
-                    expect(res).to.have.status(400);
-                    done();
-                });
-        });
-    });
+    it('TC-301-2 Gebruiker is niet ingelogd', (done) => {
+        const mealData = {
+            name: 'Pasta Bolognese',
+            description: 'Heerlijke pasta met bolognesesaus',
+            price: 8.5,
+            dateTime: '2024-05-26 18:00:00',
+            maxAmountOfParticipants: 10,
+            imageUrl: 'https://example.com/image.jpg',
+            isActive: true,
+            isVega: false,
+            isVegan: false,
+            isToTakeHome: true,
+            allergenes: 'gluten'
+        }
 
-    it('TC-302-2 Niet ingelogd', (done) => {
         chai.request(server)
-            .put('/api/meals/1')
-            .send({ name: 'Pizza', price: 9.99, maxAmountOfParticipants: 10 })
+            .post('/api/meal')
+            .send(mealData)
             .end((err, res) => {
-                expect(res).to.have.status(401);
-                done();
-            });
-    });
+                if (err) {
+                    console.error('Error creating meal:', err)
+                }
 
-    it('TC-302-3 Niet de eigenaar van de data', (done) => {
-        getValidToken((err, token) => {
-            if (err) return done(err);
-            chai.request(server)
-                .put('/api/meals/2')
-                .set('Authorization', `Bearer ${token}`)
-                .send({ name: 'Pizza', price: 9.99, maxAmountOfParticipants: 10 })
-                .end((err, res) => {
-                    expect(res).to.have.status(403);
-                    done();
-                });
-        });
-    });
+                expect(res).to.have.status(401)
+                expect(res.body).to.be.an('object')
+                expect(res.body).to.have.property('message').that.is.a('string')
+                expect(res.body.message).to.equal('No token provided!')
+                expect(res.body).to.have.property('data').that.is.empty
+                done()
+            })
+    })
 
-    it('TC-302-4 Maaltijd bestaat niet', (done) => {
-        getValidToken((err, token) => {
-            if (err) return done(err);
-            chai.request(server)
-                .put('/api/meals/9999')
-                .set('Authorization', `Bearer ${token}`)
-                .send({ name: 'Pizza', price: 9.99, maxAmountOfParticipants: 10 })
-                .end((err, res) => {
-                    expect(res).to.have.status(404);
-                    done();
-                });
-        });
-    });
+    it('TC-301-3 Maaltijd succesvol aangemaakt', (done) => {
+        const token = jwt.sign({ id: 1 }, jwtSecretKey, { expiresIn: '1h' })
 
-    it('TC-302-5 Maaltijd succesvol gewijzigd', (done) => {
-        getValidToken((err, token) => {
-            if (err) return done(err);
-            chai.request(server)
-                .put('/api/meals/1')
-                .set('Authorization', `Bearer ${token}`)
-                .send({ name: 'Pizza', price: 9.99, maxAmountOfParticipants: 10 })
-                .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    done();
-                });
-        });
-    });
-});
+        const mealData = {
+            name: 'Pasta Bolognese',
+            description: 'Heerlijke pasta met bolognesesaus',
+            price: 8.5,
+            dateTime: '2024-05-26 18:00:00',
+            maxAmountOfParticipants: 10,
+            imageUrl: 'https://example.com/image.jpg'
+        }
 
-describe('UC-303 Opvragen van alle maaltijden', () => {
-    it('TC-303-1 Lijst van maaltijden geretourneerd', (done) => {
+        // Voeg een nieuwe maaltijd toe
         chai.request(server)
-            .get('/api/meals')
+            .post('/api/meal')
+            .set('Authorization', `Bearer ${token}`)
+            .send(mealData)
             .end((err, res) => {
-                expect(res).to.have.status(200);
-                expect(res.body.data).to.be.an('array');
-                done();
-            });
-    });
-});
+                if (err) {
+                    console.error('Error creating meal:', err)
+                    return done(err)
+                }
 
-describe('UC-304 Opvragen van maaltijd bij ID', () => {
-    it('TC-304-1 Maaltijd bestaat niet', (done) => {
-        getValidToken((err, token) => {
-            if (err) return done(err);
-            chai.request(server)
-                .get('/api/meals/9999')
-                .set('Authorization', `Bearer ${token}`)
-                .end((err, res) => {
-                    expect(res).to.have.status(404);
-                    done();
-                });
-        });
-    });
+                expect(res).to.have.status(200)
+                expect(res.body).to.be.an('object')
+                expect(res.body).to.have.property('data').that.is.an('object')
+                expect(res.body.data).to.have.property('cookId').that.equals(1)
 
-    it('TC-304-2 Details van maaltijd geretourneerd', (done) => {
-        getValidToken((err, token) => {
-            if (err) return done(err);
-            chai.request(server)
-                .get('/api/meals/1')
-                .set('Authorization', `Bearer ${token}`)
-                .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.body.data).to.be.an('object');
-                    done();
-                });
-        });
-    });
-});
+                const mealId = res.body.data.id
 
-describe('UC-305 Verwijderen van maaltijd', () => {
-    it('TC-305-1 Niet ingelogd', (done) => {
-        chai.request(server)
-            .delete('/api/meals/1')
-            .end((err, res) => {
-                expect(res).to.have.status(401);
-                done();
-            });
-    });
+                // Verwijder de gecreëerde maaltijd
+                chai.request(server)
+                    .delete(`/api/meal/${mealId}`)
+                    .set('Authorization', `Bearer ${token}`)
+                    .end((err, res) => {
+                        if (err) {
+                            console.error('Error deleting meal:', err)
+                            return done(err)
+                        }
 
-    it('TC-305-2 Niet de eigenaar van de data', (done) => {
-        getValidToken((err, token) => {
-            if (err) return done(err);
-            chai.request(server)
-                .delete('/api/meals/2')
-                .set('Authorization', `Bearer ${token}`)
-                .end((err, res) => {
-                    expect(res).to.have.status(403);
-                    done();
-                });
-        });
-    });
-
-    it('TC-305-3 Maaltijd bestaat niet', (done) => {
-        getValidToken((err, token) => {
-            if (err) return done(err);
-            chai.request(server)
-                .delete('/api/meals/9999')
-                .set('Authorization', `Bearer ${token}`)
-                .end((err, res) => {
-                    expect(res).to.have.status(404);
-                    done();
-                });
-        });
-    });
-
-    it('TC-305-4 Maaltijd succesvol verwijderd', (done) => {
-        getValidToken((err, token) => {
-            if (err) return done(err);
-            chai.request(server)
-                .delete('/api/meals/1')
-                .set('Authorization', `Bearer ${token}`)
-                .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    done();
-                });
-        });
-    });
-});
+                        expect(res).to.have.status(200)
+                        done()
+                    })
+            })
+    })
+})
